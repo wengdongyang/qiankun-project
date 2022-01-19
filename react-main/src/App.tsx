a386b79e6a5a9ae277ced3fc18ac929fb6e21995
dongyang.weng <dongyang.weng@aseit.cn>
2022-01-19 14:14:59 +0800

    改用historyRouter

--- react-main/src/App.tsx
+++ react-main/src/App.tsx
@@ -2,11 +2,11 @@
  * @Author: wdy
  * @Date: 2022-01-18 18:08:57
  * @Last Modified by: wdy
- * @Last Modified time: 2022-01-18 18:27:08
+ * @Last Modified time: 2022-01-19 14:13:17
  */
 import 'minireset.css/minireset.css';
 import React from 'react';
-import {HashRouter, Switch} from 'react-router-dom';
+import {BrowserRouter, Switch} from 'react-router-dom';
 // apis
 // hooks
 // utils
@@ -18,13 +18,13 @@ import routes from './routes';
 import {HsdRoute} from '@src/components';
 const App = () => {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Switch>
         {routes.map((route, index) => (
           <HsdRoute key={index} {...route} />
         ))}
       </Switch>
-    </HashRouter>
+    </BrowserRouter>
   );
 };
 
