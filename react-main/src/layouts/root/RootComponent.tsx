26037a952f7d23383066380f8250746e0d76bc29
dongyang.weng <dongyang.weng@aseit.cn>
2022-01-19 11:22:14 +0800

    root部分

--- react-main/src/layouts/root/RootComponent.tsx
+++ react-main/src/layouts/root/RootComponent.tsx
@@ -2,20 +2,38 @@
  * @Author: wdy
  * @Date: 2021-09-23 17:22:55
  * @Last Modified by: wdy
- * @Last Modified time: 2022-01-18 18:24:03
+ * @Last Modified time: 2022-01-19 11:20:33
  */
+import styles from './RootComponent.module.less';
 import React, {Fragment} from 'react';
+import {Layout} from 'antd';
 // apis
 // hooks
 // utils
 // types
-import type {TypeHsRoute} from '@src/types';
 import type {FunctionComponent} from 'react';
+import type {TypeHsRouteComponentProps} from '@src/types';
 // stores
 // configs
 // components
-interface Props extends TypeHsRoute {}
+interface Props extends TypeHsRouteComponentProps {}
+const {Header, Content, Footer, Sider} = Layout;
 const RootComponent: FunctionComponent<Props> = props => {
-  return <section>root</section>;
+  return (
+    <Layout className={styles['main-layout']}>
+      <Sider className={styles['main-layout-sider']} collapsible>
+        <div className='logo' />
+      </Sider>
+      <Layout className={styles['layout']}>
+        <Header className={styles['layout-header']} />
+        <Content className={styles['layout-content']}>
+          <div className='site-layout-background' style={{padding: 24, minHeight: 360}}>
+            Bill is a cat.
+          </div>
+        </Content>
+        <Footer className={styles['layout-footer']}>Ant Design ©2018 Created by Ant UED</Footer>
+      </Layout>
+    </Layout>
+  );
 };
 export default RootComponent;
--- react-main/src/layouts/root/index.tsx
+++ react-main/src/layouts/root/index.tsx
@@ -1,21 +1,15 @@
-/*
- * @Author: wdy
- * @Date: 2021-09-23 17:22:55
- * @Last Modified by: wdy
- * @Last Modified time: 2022-01-18 18:23:58
- */
-import React, {Fragment} from 'react';
+import React from 'react';
 // apis
 // hooks
 // utils
 // types
 import type {FunctionComponent} from 'react';
-import type {TypeHsRoute} from '@src/types';
+import type {TypeHsRouteComponentProps} from '@src/types';
 // stores
 // configs
 // components
 import RootComponent from './RootComponent';
-interface Props extends TypeHsRoute {}
+interface Props extends TypeHsRouteComponentProps {}
 const Root: FunctionComponent<Props> = props => {
   return <RootComponent {...props} />;
 };
