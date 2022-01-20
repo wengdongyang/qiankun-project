c54f319349fbebff22b7f4eb401721be108efe36
dongyang.weng <dongyang.weng@aseit.cn>
2022-01-20 10:25:15 +0800

    type更新

---
+++ react-main/src/types/store.d.ts
@@ -0,0 +1,11 @@
+/*
+ * @Author: wdy
+ * @Date: 2022-01-20 09:49:27
+ * @Last Modified by: wdy
+ * @Last Modified time: 2022-01-20 09:49:49
+ */
+import type {TypeUserInfo} from './userInfo';
+interface TypeStore {
+  userInfo: TypeUserInfo;
+}
+export type {TypeStore};
